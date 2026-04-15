90b3d2baaa7589046b01d361d9de5318ab5bf810
Ugo-Pas <ugo.pascallon@epitech.eu>
2026-04-15 15:34:18 +0200

    [~] fix cwd

--- src/special_variable/my_cwd.c
+++ src/special_variable/my_cwd.c
@@ -9,11 +9,9 @@
 
 int my_cwd(tcsh_t *term, char **cmd)
 {
-    char *path = getcwd(NULL, 0);
+    char *path = take_value(term->env, "PWD");
 
     write(1, path, my_strlen(path));
     write(1, "\n", 1);
-    if (path != NULL)
-        free(path);
     return SUCCESS_EXIT;
 }
