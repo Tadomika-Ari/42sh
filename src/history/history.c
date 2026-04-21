2231a32b788575498075914f7d312cba9fc206d1
Ugo-Pas <ugo.pascallon@epitech.eu>
2026-04-21 13:17:59 +0200

    [+] add cmd !!

--- src/history/history.c
+++ src/history/history.c
@@ -30,6 +30,10 @@ int push_to_history(tcsh_t *term, char *cmd)
 {
     nodes_t *tmp = NULL;
 
+    if (term == NULL || cmd == NULL)
+        return FAILURE_EXIT;
+    if (my_strcmp(cmd, "!!\n") == 0)
+        return SUCCESS_EXIT;
     term->len_history++;
     tmp = init_node_history(cmd, term->len_history);
     if (tmp == NULL)
@@ -45,13 +49,38 @@ static void print_struc_history(history_t *tmp)
     write(1, tmp->cmd, my_strlen(tmp->cmd));
 }
 
+int my_last_cmd(tcsh_t *termn, char **cmd)
+{
+    history_t *tmp = NULL;
+    nodes_t *current = NULL;
+
+    if (termn == NULL || termn->history == NULL) {
+        printf("0: Event not found.\n");
+        return FAILURE_EXIT;
+    }
+    current = termn->history;
+    while (current->next != NULL)
+        current = current->next;
+    if (current->data != NULL) {
+        tmp = current->data;
+        return choose_command(termn, tmp->cmd);
+    } else {
+        printf("0: Event not found.\n");
+        return FAILURE_EXIT;
+    }
+}
+
 int my_history(tcsh_t *term, char **cmd)
 {
     history_t *info = NULL;
 
+    if (term == NULL)
+        return FAILURE_EXIT;
     for (nodes_t *current = term->history; current != NULL;
         current = current->next) {
         info = current->data;
+        if (info == NULL)
+            continue;
         print_struc_history(info);
     }
     return SUCCESS_EXIT;
