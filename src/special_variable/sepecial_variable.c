4848a6faa10a18df0a3507ce1eec537dc7257d43
Ugo-Pas <ugo.pascallon@epitech.eu>
2026-04-22 13:35:03 +0200

    [~] update variable environement if $

--- src/special_variable/sepecial_variable.c
+++ src/special_variable/sepecial_variable.c
@@ -11,11 +11,13 @@ int sepecial_variable(tcsh_t *term, char *cmd)
 {
     char *path = NULL;
 
+    if (cmd[0] != '$')
+        return -1;
     my_strupcase(cmd);
-    if (my_strcmp("CWD", cmd) == 0)
+    if (my_strcmp("$CWD", cmd) == 0)
         path = take_value(term->env, "PWD");
     else
-        path = take_value(term->env, cmd);
+        path = take_value(term->env, cmd + 1);
     if (path == NULL)
         return -1;
     write(1, path, my_strlen(path));
