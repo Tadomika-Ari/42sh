7b0706f06bb8e284a7f5785e8659ee58aeeb9794
Alessandro Paris <alessandro1.paris@epitech.eu>
2026-04-25 14:21:04 +0200

    [+] add create job function

--- src/jobs/add_job.c
+++ src/jobs/add_job.c
@@ -21,6 +21,24 @@ int get_max_job_id(tcsh_t *term)
     return max;
 }
 
+job_t *create_job(tcsh_t *term, pid_t pgid, char *cmd, job_state_t state)
+{
+    job_t *new_job = malloc(sizeof(job_t));
+
+    if (!new_job)
+        return NULL;
+    new_job->cmd = my_strdup(cmd);
+    if (!new_job->cmd) {
+        free(new_job);
+        return NULL;
+    }
+    new_job->pgid = pgid;
+    new_job->state = state;
+    new_job->id = get_max_job_id(term) + 1;
+    new_job->next = NULL;
+    return new_job;
+}
+
 void add_job(tcsh_t *term, pid_t pgid, char *cmd, job_state_t state)
 {
     job_t *new_job = NULL;
@@ -28,18 +46,7 @@ void add_job(tcsh_t *term, pid_t pgid, char *cmd, job_state_t state)
 
     if (!term || !cmd)
         return;
-    new_job = malloc(sizeof(job_t));
-    if (!new_job)
-        return;
-    new_job->cmd = my_strdup(cmd);
-    if (!new_job->cmd) {
-        free(new_job);
-        return;
-    }
-    new_job->pgid = pgid;
-    new_job->state = state;
-    new_job->id = get_max_job_id(term) + 1;
-    new_job->next = NULL;
+    new_job = create_job(term, pgid, cmd, state);
     if (!term->jobs) {
         term->jobs = new_job;
         return;
