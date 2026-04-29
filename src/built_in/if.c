2b0e663d4353fdaa8a80cc69c73fa954e3c56c3f
alcibiade Streit-Soumaila <alcibiade.streit-soumaila@epitech.eu>
2026-04-29 14:06:13 +0200

    [~] correct set and if

--- src/built_in/if.c
+++ src/built_in/if.c
@@ -6,6 +6,9 @@
 */
 
 #include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "../../include/struct.h"
 
 static int fallback_cond(char *expr)
@@ -20,75 +23,46 @@ static int fallback_cond(char *expr)
     return 0;
 }
 
-static char *search_buff(int fd, char *buff, char *tmp)
+static char *join_expr(char **argv)
 {
-    char *new_buff = NULL;
+    size_t len = 0;
+    size_t pos = 0;
+    char *expr = NULL;
 
-    for (int i = 0; read(fd, tmp, 1) > 0; i++) {
-        new_buff = realloc(buff, i + 2);
-        if (new_buff == NULL) {
-            free(buff);
-            free(tmp);
-            return NULL;
-        }
-        buff = new_buff;
-        buff[i] = tmp[0];
-        buff[i + 1] = '\0';
-    }
-    free(tmp);
-    return buff;
-}
-
-static char *get_fd(int fd)
-{
-    char *buff = malloc(sizeof(char) * 1);
-    char *tmp = malloc(sizeof(char) * 1);
-
-    if (buff == NULL || tmp == NULL) {
-        free(buff);
-        free(tmp);
+    if (argv == NULL)
         return NULL;
+    for (int i = 0; argv[i] != NULL && my_strcmp(argv[i], "then") != 0; i++)
+        len += strlen(argv[i]) + 1;
+    expr = malloc(len + 1);
+    if (expr == NULL)
+        return NULL;
+    for (int i = 0; argv[i] != NULL && my_strcmp(argv[i], "then") != 0; i++) {
+        memcpy(expr + pos, argv[i], strlen(argv[i]));
+        pos += strlen(argv[i]);
+        if (argv[i + 1] != NULL && my_strcmp(argv[i + 1], "then") != 0) {
+            expr[pos] = ' ';
+            pos++;
+        }
     }
-    buff[0] = '\0';
-    return search_buff(fd, buff, tmp);
-}
-
-static int true_cond(int fd, bool *error)
-{
-    char *cond = get_fd(fd);
-    int res = 0;
-
-    if (cond == NULL) {
-        *error = true;
-        return ALTERNATIVE_EXIT;
-    }
-    res = atoi(cond);
-    free(cond);
-    return res;
+    expr[pos] = '\0';
+    return expr;
 }
 
 static int search_condition(tcsh_t *term, char **argv, bool *error)
 {
-    int pid = fork();
-    int status = 0;
-    int fd[2] = {-1, -1};
+    char *expr = join_expr(argv);
+    int cond = 0;
 
-    pipe(fd);
-    if (pid == 0) {
-        dup2(fd[1], STDOUT_FILENO);
-        dup2(fd[1], STDERR_FILENO);
-        close(fd[0]);
-        close(fd[1]);
-        execve("/bin/calc", (char *[]){"calc", argv[0], NULL},
-            node_to_array(term->env));
-        _exit(1);
-    }
-    close(fd[1]);
-    if (waitpid(pid, &status, 0) < 0) {
+    (void)term;
+    if (expr == NULL) {
         *error = true;
         return ALTERNATIVE_EXIT;
     }
-    return true_cond(fd[0], error);
+    cond = fallback_cond(expr);
+    if (cond == 0 && atoi(expr) != 0)
+        cond = 1;
+    free(expr);
+    return cond;
 }
 
 static int is_then(char **argv)
@@ -112,6 +86,8 @@ static int is_then(char **argv)
 
 static int stop(char **argv)
 {
+    if (argv == NULL || argv[0] == NULL)
+        return ALTERNATIVE_EXIT;
     if (my_strcmp("else", argv[0]) == 0) {
         if (argv[1] == NULL)
             return SUCCESS_EXIT;
@@ -130,30 +106,46 @@ int else_if(tcsh_t *term, char **argv)
 
     if (error == true || then == -1)
         return ALTERNATIVE_EXIT;
-    if ((cond != 0 && then == 1) || (cond == 0 && then == 0))
+    if ((cond != 0 && then == 1))
         return SUCCESS_EXIT;
-    if (cond != 0 && then == 0)
-        return search_command(term, argv + 1, NULL);
+    if (cond != 0 && then == 0) {
+        search_command(term, argv + 1, NULL);
+        return SUCCESS_EXIT;
+    }
     return ALTERNATIVE_EXIT;
 }
 
-int in_if(tcsh_t *term)
+static char **get_info(void)
 {
     size_t len = 0;
     char *lign = NULL;
     char **tmp = NULL;
 
+    if (getline(&lign, &len, stdin) == -1) {
+        free(lign);
+        return NULL;
+    }
+    tmp = parser3000(lign, " \t\n");
+    if (tmp == NULL || tmp[0] == NULL) {
+        free_array(tmp);
+        free(lign);
+        return NULL;
+    }
+    free(lign);
+    return tmp;
+}
+
+int in_if(tcsh_t *term)
+{
+    char **tmp = NULL;
+
     while (1) {
-        if (getline(&lign, &len, stdin) == -1) {
-            free(lign);
-            return ALTERNATIVE_EXIT;
-        }
-        tmp = my_str_to_word_array(lign, " ");
+        tmp = get_info();
         if (stop(tmp) == 0) {
             free_array(tmp);
             return SUCCESS_EXIT;
         }
-        if (else_if(term, tmp) != 0) {
+        if (strcmp(tmp[0], "else") == 0 && else_if(term, tmp + 2) == 0) {
             free_array(tmp);
             return SUCCESS_EXIT;
         }
--- src/built_in/set.c
+++ src/built_in/set.c
@@ -19,6 +19,44 @@ void insert(tcsh_t *term, locals_t *local)
     push_front(&term->locals, new_node(local));
 }
 
+static char *take_name(char *var)
+{
+    char **tmp = my_str_to_word_array(var, "=");
+    char *res = NULL;
+
+    if (tmp == NULL)
+        return NULL;
+    if (correct_name(tmp[0], "set") != SUCCESS_EXIT) {
+        free_array(tmp);
+        return NULL;
+    }
+    res = my_strdup(tmp[0]);
+    free_array(tmp);
+    return res;
+}
+
+int single_value(tcsh_t *term, char *var)
+{
+    locals_t *local = malloc(sizeof(locals_t));
+
+    if (local == NULL)
+        return -1;
+    local->name = take_name(var);
+    local->value = NULL;
+    if (local->name == NULL) {
+        free(local);
+        return -1;
+    }
+    for (int i = 0; var[i]; i++) {
+        if (var[i] == '=') {
+            local->value = var[i + 1] != '\0' ? my_strdup(&var[i + 1]) : NULL;
+            break;
+        }
+    }
+    insert(term, local);
+    return 1;
+}
+
 int add_variable(char **argv, tcsh_t *term)
 {
     int res = 1;
@@ -32,7 +70,7 @@ int add_variable(char **argv, tcsh_t *term)
         res += 1;
     else {
         push_front(&term->locals, new_node(local));
-        return res;
+        return 1;
     }
     if (argv[1] != NULL && argv[2] != NULL) {
         local->value = my_strdup(argv[2]);
@@ -42,12 +80,48 @@ int add_variable(char **argv, tcsh_t *term)
     return res;
 }
 
+int correct_var(char *name, char *cmd)
+{
+    if ((name[0] < 'a' || name[0] > 'z') &&
+        (name[0] < 'A' || name[0] > 'Z') && name[0] != '_')
+        return ALTERNATIVE_EXIT;
+    if (my_str_name(name) != 0)
+        return ALTERNATIVE_EXIT;
+    return SUCCESS_EXIT;
+}
+
+static int display(tcsh_t *term)
+{
+    for (nodes_t *tmp = term->locals; tmp; tmp = tmp->next) {
+        write(1, ((locals_t *)tmp->data)->name, my_strlen(((locals_t *)tmp->data)->name));
+        if (((locals_t *)tmp->data)->value) {
+            write(1, "\t", 1);
+            write(1, ((locals_t *)tmp->data)->value, my_strlen(((locals_t *)tmp->data)->value));
+        }
+        write(1, "\n", 1);
+    }
+    return SUCCESS_EXIT;
+}
+
 int my_set(tcsh_t *term, char **argv)
 {
+    int tmp = 0;
+
+    if (len_array(argv) == 0)
+        return display(term);
     for (int i = 0; argv[i];) {
-        if (correct_name(argv[i], "set") != SUCCESS_EXIT)
+        if (correct_var(argv[i], "set") != SUCCESS_EXIT) {
+            tmp = single_value(term, argv[i]);
+            if (tmp == 1) {
+                i += tmp;
+                continue;
+            }
             return ALTERNATIVE_EXIT;
-        i += add_variable(&argv[i], term);
+        }
+        tmp = add_variable(&argv[i], term);
+        if (tmp == -1)
+            return ALTERNATIVE_EXIT;
+        i += tmp;
     }
     return SUCCESS_EXIT;
 }
