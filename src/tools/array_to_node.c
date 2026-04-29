6f95905fd7ef6a182e6df6bf0ee8060275af78fc
Alessandro Paris <alessandro1.paris@epitech.eu>
2026-04-29 15:34:17 +0200

    [~] update: move is sep and add show array function

--- src/tools/array_to_node.c
+++ src/tools/array_to_node.c
@@ -7,6 +7,25 @@
 
 #include "../../include/struct.h"
 
+int is_sep(char c, char *sep)
+{
+    for (int i = 0; sep[i] != '\0'; i++) {
+        if (sep[i] == c) {
+            return TRUE;
+        }
+    }
+    return FALSE;
+}
+
+void show_array(char **tab)
+{
+    if (!tab)
+        return;
+    for (int i = 0; tab[i] != NULL; i++) {
+        printf("%d %s\n", i, tab[i]);
+    }
+}
+
 nodes_t *array_to_node(char **array)
 {
     nodes_t *list = NULL;
