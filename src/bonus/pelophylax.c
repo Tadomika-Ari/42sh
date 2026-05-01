19243cd6bb167d8d524685716eeb264b9906e5a0
Alessandro Paris <alessandro1.paris@epitech.eu>
2026-05-01 18:26:39 +0200

    [+] add: fill pelophylax

--- src/bonus/pelophylax.c
+++ src/bonus/pelophylax.c
@@ -7,8 +7,31 @@
 
 #include "../../include/struct.h"
 
+char *fill_pelophylax(void)
+{
+    int fd = open(PELOPHYLAX, O_RDONLY);
+    char *buff = NULL;
+    struct stat st;
+    int size = 0;
+
+    if (fd == -1)
+        return NULL;
+    if (stat(PELOPHYLAX, &st) == -1)
+        return NULL;
+    buff = malloc(sizeof(char) * (st.st_size + 1));
+    size = read(fd, buff, st.st_size);
+    buff[size] = '\0';
+    close(fd);
+    return buff;
+}
+
 int pelophylax(tcsh_t *term, char **argv)
 {
-    printf("\033[32mPELOPHYLAX KL ESCULENTUS\033[0m\n");
+    char *buff = fill_pelophylax();
+
+    if (!buff)
+        return FAILURE_EXIT;
+    printf("\033[32m%s\033[0m\n", buff);
+    free(buff);
     return SUCCESS_EXIT;
 }
