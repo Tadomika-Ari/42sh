73caf0a8790e44b70341c02ae397feee7c8695ba
Alessandro Paris <alessandro1.paris@epitech.eu>
2026-05-01 09:47:52 +0200

    [+] add: pelophylax file and function

---
+++ src/bonus/pelophylax.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2026
+** pelophylax
+** File description:
+** pelophylax
+*/
+
+#include "../../include/struct.h"
+
+int pelophylax(tcsh_t *term, char **argv)
+{
+    printf("PELOPHYLAX KL ESCULENTUS\n");
+    return SUCCESS_EXIT;
+}
