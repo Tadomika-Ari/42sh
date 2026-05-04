22c5f4734db45b875149f4ca16b8384fdff14375
Alessandro Paris <alessandro1.paris@epitech.eu>
2026-05-04 16:08:03 +0200

    [+] add: print hangman body and current HP

--- src/bonus/print_letter_hangman.c
+++ src/bonus/print_letter_hangman.c
@@ -9,7 +9,7 @@
 
 void print_letter_hangman(hang_t *hang)
 {
-    printf("%s\n", ASK_LETTER);
+    printf("You currently have %d HP, %s\n", hang->hp, ASK_LETTER);
     for (int r = 0; r < NB_ROW; r++)
         printf("%s\n", STEPS[10 - hang->hp][r]);
 }
