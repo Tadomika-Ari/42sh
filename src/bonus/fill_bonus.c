29ff6f318bd0481ca8f441c09176569ef3f95320
Alessandro Paris <alessandro1.paris@epitech.eu>
2026-05-04 10:22:49 +0200

    [+] add: fill bonus in bonus folder

---
+++ src/bonus/fill_bonus.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2026
+** fill
+** File description:
+** bonus
+*/
+
+#include "../../include/struct.h"
+
+int fill_bonus(tcsh_t *term)
+{
+    if (push_function(term, pelophylax, "pelophylax") == FAILURE_EXIT)
+        return FAILURE_EXIT;
+    return EXIT_SUCCESS;
+}
