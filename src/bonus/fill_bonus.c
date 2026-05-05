0a4d36f80402df41a9eb5d4c160d12d2cfa14565
Ugo-Pas <ugo.pascallon@epitech.eu>
2026-05-05 11:26:47 +0200

    [~] fix cmd tic tac toe add _

--- src/bonus/fill_bonus.c
+++ src/bonus/fill_bonus.c
@@ -11,7 +11,7 @@ int fill_bonus(tcsh_t *term)
 {
     if (push_function(term, pelophylax, "pelophylax") == FAILURE_EXIT)
         return FAILURE_EXIT;
-    if (push_function(term, tic_tac_toe, "tic tac toe") == FAILURE_EXIT)
+    if (push_function(term, tic_tac_toe, "tic_tac_toe") == FAILURE_EXIT)
         return FAILURE_EXIT;
     return EXIT_SUCCESS;
 }
