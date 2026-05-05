6c684670a2ea76d82cb81709f49d19d00762473a
Alessandro Paris <alessandro1.paris@epitech.eu>
2026-05-05 10:05:09 +0200

    [+] add: visualisation , flag help, v and visualisation

--- src/bonus/flipcoin.c
+++ src/bonus/flipcoin.c
@@ -7,13 +7,32 @@
 
 #include "../../include/struct.h"
 
+int help_flipcoin(void)
+{
+    char *buff = fill_buff_bonus(FLIPCOIN);
+
+    if (!buff)
+        return FAILURE_EXIT;
+    printf("%s\n", buff);
+    free(buff);
+    return SUCCESS_EXIT;
+}
+
 int flipcoin(tcsh_t *term, char **argv)
 {
     int odds = rand() % 2;
+    int len = len_array(argv);
+    int tmp = 0;
 
-    if (odds == 0)
-        printf("Tail\n");
-    else
-        printf("Head\n");
+    if (len == 1 && (my_strcmp("-h", argv[0]) == 0)
+        || my_strcmp("--help", argv[0]) == 0)
+        return help_flipcoin();
+    if (len == 0) {
+        tmp = (odds == 0) ? (printf("Tail\n")) : printf("Head\n");
+    }
+    if (len == 1 && (my_strcmp("--visualisation", argv[0]) == 0)
+        || my_strcmp("-v", argv[0]) == 0) {
+        tmp = (odds == 0) ? printf("%s", TAIL) : printf("%s", HEAD);
+    }
     return SUCCESS_EXIT;
 }
