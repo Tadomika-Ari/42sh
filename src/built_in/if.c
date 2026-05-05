943c60c41973af1c557ac9790ca26589fcc3d290
alcibiade Streit-Soumaila <alcibiade.streit-soumaila@epitech.eu>
2026-05-05 18:15:49 +0200

    [~] mod change position of child function in tools if

--- src/built_in/if.c
+++ src/built_in/if.c
@@ -20,28 +20,6 @@ static int apply(tcsh_t *term, char **cmd)
     return normalize(term, NULL, cmd, status);
 }
 
-int child(tcsh_t *term, int fd[2], char *cond)
-{
-    int pid = fork();
-    char **env = node_to_array(term->env);
-    int res = 0;
-
-    if (pid == 0) {
-        close(fd[0]);
-        dup2(fd[1], STDOUT_FILENO);
-        close(STDERR_FILENO);
-        close(fd[1]);
-        execve("/usr/bin/calc", (char *[]){"calc", cond, NULL}, env);
-        exit(-1);
-    }
-    waitpid(pid, &res, 0);
-    if (env)
-        free(env);
-    if (!WIFEXITED(res) || WEXITSTATUS(res) != 0)
-        return my_cmd_error(": Expression Syntax.\n", "if", ALTERNATIVE_EXIT);
-    return SUCCESS_EXIT;
-}
-
 static char *read_fd(int pipefd[2])
 {
     char buf[1024];
