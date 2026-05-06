134ca9822c38f7d43ea31eb6c8d73c2035463482
galypack <galya1.packer@epitech.eu>
2026-05-06 14:59:02 +0200

    [+] add: new parsing method for error cases

--- src/jobs/job_control.c
+++ src/jobs/job_control.c
@@ -49,6 +49,43 @@ char *is_job_control(parse_t *parse, char *cmd, int i)
     return NULL;
 }
 
+int len_job_cmd(char *cmd)
+{
+    parse_t parse = {0};
+    int nb = 0;
+    for (int i = 0; cmd[i] != '\0'; i++) {
+        update_state(&parse, cmd[i]);
+        if (is_job_control(&parse, cmd, i) != NULL) {
+            nb = nb + 1;
+            i = i + 1;
+        }
+    }
+    return nb;
+}
+
+void job_cmd_splitting(char *cmd, char **commands, char **jobs)
+{
+    parse_t parse = {0};
+    int position = 0;
+    int nb_cmd = 0;
+    char *separators = NULL;
+
+    for (int i = 0; cmd[i] != '\0'; i++){
+        update_state(&parse, cmd[i]);
+        separators = is_job_control(&parse, cmd, i);
+        if (separators != NULL){
+            commands[nb_cmd] = strndup(cmd + position, i - position);
+            jobs[nb_cmd] = separators;
+            position = i + 2;
+            nb_cmd++;
+            i++;
+        }
+    }
+    commands[nb_cmd] = strndup(cmd + position, my_strlen(cmd) - position);
+    commands[nb_cmd + 1] = NULL;
+    jobs[nb_cmd ] = NULL;
+}
+
 int job_detection(char *cmd)
 {
     parse_t parse = {0};
