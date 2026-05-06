5b64c1c124b09299bc263c4d243c31cdb0b9122c
galypack <galya1.packer@epitech.eu>
2026-05-06 14:52:13 +0200

    [+] add: job && function

--- src/jobs/job_control.c
+++ src/jobs/job_control.c
@@ -24,6 +24,22 @@ int or_job(tcsh_t *term, char *cmd)
     return value;
 }
 
+int and_job(tcsh_t *term, char *cmd)
+{
+    char **array_cmd = my_strtwabw(cmd, "&&");
+    int value = 0;
+
+    for (int i = 0; array_cmd[i] != NULL; i++){
+        value = or_job(term, array_cmd[i]);
+        if (value != SUCCESS_EXIT){
+            free_array(array_cmd);
+            return value;
+        }
+    }
+    free_array(array_cmd);
+    return SUCCESS_EXIT;
+}
+
 char *is_job_control(parse_t *parse,char *cmd, int i)
 {
     if (!is_protected(parse) && (cmd[i] == '&' && cmd[i + 1] == '&'))
