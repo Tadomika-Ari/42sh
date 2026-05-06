16ee4d5720dd9c96fa291d92b77f274be972da8a
galypack <galya1.packer@epitech.eu>
2026-05-06 15:32:31 +0200

    [+] add: functions to struct.h

--- include/struct.h
+++ include/struct.h
@@ -527,4 +527,8 @@ int check_quotes(char *str);
 int check_back(char *str);
 
 int job_control(tcsh_t *term, char *cmd);
+
+int empty_error_case(char **commands, char **jobs);
+
+int empty_cmd_detect(char *cmd);
 #endif
