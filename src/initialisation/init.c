5bec06c7c29efd3aaaaa2cde70d5e1179bbd8ec6
alcibiade Streit-Soumaila <alcibiade.streit-soumaila@epitech.eu>
2026-05-06 12:56:01 +0200

    [+] add function where in main term

--- src/initialisation/init.c
+++ src/initialisation/init.c
@@ -60,6 +60,8 @@ static int fill_annexe(tcsh_t *term)
         return FAILURE_EXIT;
     if (push_function(term, my_which, "which") == FAILURE_EXIT)
         return FAILURE_EXIT;
+    if (push_function(term, my_where, "where") == FAILURE_EXIT)
+        return FAILURE_EXIT;
     return SUCCESS_EXIT;
 }
 
