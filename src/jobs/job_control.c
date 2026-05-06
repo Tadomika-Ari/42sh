00055e019273b3c8a94ed3a78d8556aef6314599
galypack <galya1.packer@epitech.eu>
2026-05-06 15:22:21 +0200

    [+] add: dividing function for coding style

--- src/jobs/job_control.c
+++ src/jobs/job_control.c
@@ -8,6 +8,12 @@
 #include "../../include/struct.h"
 #include "../../lib/my/my.h"
 
+static void free_cmd_jobs(char **commands, char **jobs)
+{
+    free_array(commands);
+    free(jobs);
+}
+
 static char *is_job_control(parse_t *parse, char *cmd, int i)
 {
     if (!is_protected(parse) && (cmd[i] == '&' && cmd[i + 1] == '&'))
@@ -34,55 +40,82 @@ static int len_job_cmd(char *cmd)
     return nb;
 }
 
-void job_cmd_splitting(char *cmd, char **commands, char **jobs)
+int fill_commands_jobs(jobs_cont_t *counts, char *cmd,
+    char **commands, char **jobs)
 {
-    parse_t parse = {0};
-    int position = 0;
-    int nb_cmd = 0;
-    char *separators = NULL;
-
-    if (cmd == NULL || commands == NULL || jobs == NULL)
-        return;
-    for (int i = 0; cmd[i] != '\0'; i++){
-        update_state(&parse, cmd[i]);
-        separators = is_job_control(&parse, cmd, i);
-        if (separators != NULL){
-            commands[nb_cmd] = strndup(cmd + position, i - position);
-            jobs[nb_cmd] = separators;
-            position = i + 2;
-            nb_cmd++;
-            i++;
-        }
-    }
-    commands[nb_cmd] = strndup(cmd + position, my_strlen(cmd) - position);
-    commands[nb_cmd + 1] = NULL;
-    jobs[nb_cmd] = NULL;
+    commands[counts->nb_cmd] = strndup(cmd + counts->position,
+        counts->i - counts->position);
+    if (commands[counts->nb_cmd] == NULL)
+        return ALTERNATIVE_EXIT;
+    jobs[counts->nb_cmd] = counts->separators;
+    counts->position = counts->i + 2;
+    counts->nb_cmd++;
+    return SUCCESS_EXIT;
 }
 
-int job_execution(tcsh_t *term, char **commands, char **jobs)
+static int handle_separators(jobs_cont_t *counts, char *cmd,
+    char **commands, char **jobs)
 {
-    int ignore = 0;
-    int or_done = 0;
-    int value = 0;
+    if (counts->separators == NULL)
+        return SUCCESS_EXIT;
+    if (fill_commands_jobs(counts, cmd, commands, jobs) == ALTERNATIVE_EXIT)
+        return ALTERNATIVE_EXIT;
+    counts->i++;
+    return SUCCESS_EXIT;
+}
 
-    if (commands == NULL || jobs == NULL)
+int job_cmd_splitting(jobs_cont_t *counts, char *cmd,
+    char **commands, char **jobs)
+{
+    parse_t parse = {0};
+
+    if (cmd == NULL || commands == NULL || jobs == NULL)
         return FAILURE_EXIT;
-    for (int i = 0; commands[i] != NULL; i++){
-        if (ignore == FALSE && or_done == FALSE)
-            value = choose_command(term, commands[i]);
-        if (jobs[i] != NULL && my_strcmp(jobs[i], "&&") == 0 && value != 0
-            || jobs[i] != NULL && my_strcmp(jobs[i], "||") == 0 && value == 0){
-            ignore = TRUE;
-            or_done = TRUE;
-        }
-        if (jobs[i] != NULL && my_strcmp(jobs[i], "&&") == 0 && value == 0)
-            ignore = FALSE;
-        if (jobs[i] != NULL && my_strcmp(jobs[i], "||") == 0 && value != 0) {
-            ignore = FALSE;
-            or_done = FALSE;
-        }
+    for (counts->i = 0; cmd[counts->i] != '\0'; counts->i++){
+        update_state(&parse, cmd[counts->i]);
+        counts->separators = is_job_control(&parse, cmd, counts->i);
+        if (handle_separators(counts, cmd, commands, jobs) == ALTERNATIVE_EXIT)
+            return ALTERNATIVE_EXIT;
     }
-    return value;
+    commands[counts->nb_cmd] = strndup(cmd + counts->position,
+        my_strlen(cmd) - counts->position);
+    if (commands[counts->nb_cmd] == NULL)
+        return FAILURE_EXIT;
+    commands[counts->nb_cmd + 1] = NULL;
+    jobs[counts->nb_cmd] = NULL;
+    return SUCCESS_EXIT;
+}
+
+static void status_true(jobs_exec_t *status)
+{
+    status->ignore = TRUE;
+    status->or_done = TRUE;
+}
+
+static void status_false(jobs_exec_t *status)
+{
+    status->ignore = FALSE;
+    status->or_done = FALSE;
+}
+
+int job_execution(tcsh_t *term, jobs_exec_t *sta,
+    char **cmds, char **jobs)
+{
+    if (cmds == NULL || jobs == NULL)
+        return FALSE;
+    for (int i = 0; cmds[i] != NULL; i++){
+        if (sta->ignore == FALSE && sta->or_done == FALSE)
+            sta->value = choose_command(term, cmds[i]);
+        if (jobs[i] != NULL && my_strcmp(jobs[i], "&&") == 0 && sta->value != 0
+            || jobs[i] != NULL && my_strcmp(jobs[i], "||") == 0
+            && sta->value == 0)
+            status_true(sta);
+        if (jobs[i] != NULL && my_strcmp(jobs[i], "&&") == 0 && sta->value == 0)
+            sta->ignore = FALSE;
+        if (jobs[i] != NULL && my_strcmp(jobs[i], "||") == 0 && sta->value != 0)
+            status_false(sta);
+    }
+    return sta->value;
 }
 
 int handle_splitting_error(char *cmd, char **commands, char **jobs)
@@ -98,6 +131,7 @@ int handle_splitting_error(char *cmd, char **commands, char **jobs)
 
 int job_control(tcsh_t *term, char *cmd)
 {
+    jobs_exec_t status = {0};
     char **commands = NULL;
     char **jobs = NULL;
     int value = 0;
@@ -112,9 +146,9 @@ int job_control(tcsh_t *term, char *cmd)
         free(commands);
         return FAILURE_EXIT;
     }
-    job_cmd_splitting(cmd, commands, jobs);
-    value = job_execution(term, commands, jobs);
-    free_array(commands);
-    free(jobs);
+    if (handle_splitting_error(cmd, commands, jobs) == ALTERNATIVE_EXIT)
+        return ALTERNATIVE_EXIT;
+    value = job_execution(term, &status, commands, jobs);
+    free_cmd_jobs(commands, jobs);
     return value;
 }
