eb6f1af49be3d290157449cef2284ff9f8f6a645
alcibiade Streit-Soumaila <alcibiade.streit-soumaila@epitech.eu>
2026-05-07 15:38:47 +0200

    [~] mod rename of ALTERNATIVE_EXIT in ALT_EXIT

--- src/error/other_error_message.c
+++ src/error/other_error_message.c
@@ -11,29 +11,29 @@ int error_alphanumeric(char *cmd)
 {
     write(1, cmd, my_strlen(cmd));
     return put_err(": Variable name must contain alphanumeric characters.\n",
-        ALTERNATIVE_EXIT);
+        ALT_EXIT);
 }
 
 int error_not_a_directory(char *cmd)
 {
     write(1, cmd, my_strlen(cmd));
-    return put_err(": Not a directory.\n", ALTERNATIVE_EXIT);
+    return put_err(": Not a directory.\n", ALT_EXIT);
 }
 
 int argument_not_support(char *cmd)
 {
     write(1, cmd, my_strlen(cmd));
-    return put_err(": Argument not supported.\n", ALTERNATIVE_EXIT);
+    return put_err(": Argument not supported.\n", ALT_EXIT);
 }
 
 int error_no_home(char *cmd)
 {
     write(1, cmd, my_strlen(cmd));
-    return put_err(": No home directory.\n", ALTERNATIVE_EXIT);
+    return put_err(": No home directory.\n", ALT_EXIT);
 }
 
 int error_permission_denied(char *cmd)
 {
     write(1, cmd, my_strlen(cmd));
-    return put_err(": Permission denied.\n", ALTERNATIVE_EXIT);
+    return put_err(": Permission denied.\n", ALT_EXIT);
 }
