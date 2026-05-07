19a7836d2c87ebd53a85e76b7b9d9beaaf875771
Alessandro Paris <alessandro1.paris@epitech.eu>
2026-05-07 12:30:27 +0200

    [+] add: help handling for pelophylax command

--- src/bonus/pelophylax.c
+++ src/bonus/pelophylax.c
@@ -7,10 +7,26 @@
 
 #include "../../include/struct.h"
 
+int help_pelophylax(void)
+{
+    char *buff = fill_buff_bonus(PELOPHYLAX_HELP);
+
+    if (!buff)
+        return FAILURE_EXIT;
+    printf("%s\n", buff);
+    free(buff);
+    return SUCCESS_EXIT;
+}
+
 int pelophylax(tcsh_t *term, char **argv)
 {
-    char *buff = fill_buff_bonus(PELOPHYLAX);
+    char *buff = NULL;
+    int len = len_array(argv);
 
+    if (len == 1 && (my_strcmp("-h", argv[0]) == 0)
+        || my_strcmp("--help", argv[0]) == 0)
+        return help_pelophylax();
+    buff = fill_buff_bonus(PELOPHYLAX);
     if (!buff)
         return FAILURE_EXIT;
     printf("\033[32m%s\033[0m\n", buff);
