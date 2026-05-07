7075d4c8c414ff1e5cd68f3d5b3aacabed187a5a
Alessandro Paris <alessandro1.paris@epitech.eu>
2026-05-07 09:13:31 +0200

    [+] add: rickroll file and function

---
+++ src/bonus/rickroll.c
@@ -0,0 +1,25 @@
+/*
+** EPITECH PROJECT, 2026
+** rickroll
+** File description:
+** rickroll
+*/
+
+#include "../../include/struct.h"
+
+int rickroll(void)
+{
+    int odd = rand() % 100;
+    char *buff = fill_buff_bonus(RICKROLL_TXT);
+
+    if (odd <= RICKROLL_ODDS) {
+        buff = fill_buff_bonus(RICKROLL_TXT);
+        if (!buff)
+            return FAILURE_EXIT;
+        printf("\033[32m%s\033[0m\n", buff);
+        free(buff);
+        play_sound(RICKROLL);
+        return SUCCESS_EXIT;
+    }
+    return SUCCESS_EXIT;
+}
