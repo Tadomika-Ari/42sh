015e47222e63cb240a5d69379479f9ec359ca678
Ugo-Pas <ugo.pascallon@epitech.eu>
2026-05-07 09:40:54 +0200

    [+] add creat_single_str

--- src/alias/add_alias.c
+++ src/alias/add_alias.c
@@ -45,10 +45,9 @@ alias_t *init_alias(char *name, char *cmd)
     return tmp;
 }
 
-static nodes_t *init_node_alias(char **cmd, nodes_t *new)
+static char *creat_single_str(char **cmd)
 {
     char *cmd_alias = NULL;
-    alias_t *new_alias = NULL;
     int len = 0;
 
     for (int i = 1; cmd[i] != NULL; i++)
@@ -57,8 +56,22 @@ static nodes_t *init_node_alias(char **cmd, nodes_t *new)
     if (cmd_alias == NULL)
         return NULL;
     cmd_alias[0] = '\0';
-    for (int i = 1; cmd[i] != NULL; i++)
+    for (int i = 1; cmd[i] != NULL; i++) {
         my_strcat(cmd_alias, cmd[i]);
+        if (cmd[i + 1] != NULL)
+            my_strcat(cmd_alias, " ");
+    }
+    return cmd_alias;
+}
+
+static nodes_t *init_node_alias(char **cmd, nodes_t *new)
+{
+    alias_t *new_alias = NULL;
+    char *cmd_alias = NULL;
+
+    cmd_alias = creat_single_str(cmd);
+    if (cmd_alias == NULL)
+        return NULL;
     new_alias = init_alias(cmd[0], cmd_alias);
     if (new_alias == NULL) {
         free(cmd_alias);
@@ -69,16 +82,26 @@ static nodes_t *init_node_alias(char **cmd, nodes_t *new)
     return new;
 }
 
+static int get_unique_alias(tcsh_t *term, char *cmd)
+{
+    char *alias_cmd = NULL;
+
+    alias_cmd = check_alias(term, cmd);
+    if (alias_cmd != NULL) {
+        printf("%s\n", alias_cmd);
+        free(alias_cmd);
+    }
+    return SUCCESS_EXIT;
+}
+
 int my_alias(tcsh_t *term, char **cmd)
 {
     nodes_t *node = NULL;
 
     if (cmd[0] == NULL)
         return print_alias(term);
-    if (cmd[1] == NULL) {
-        check_alias(term, cmd[0]);
-        return SUCCESS_EXIT;
-    }
+    if (cmd[1] == NULL)
+        return get_unique_alias(term, cmd[0]);
     if (my_strcmp("alias", cmd[0]) == 0)
         return put_err("alias: Too dangerous to alias that.\n",
             ALTERNATIVE_EXIT);
