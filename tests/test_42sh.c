f017ee6b79bc6f71f98289fa818868ee4b362027
Alessandro Paris <alessandro1.paris@epitech.eu>
2026-05-11 16:29:43 +0200

    [+] add: my alias unique alias test

--- tests/test_42sh.c
+++ tests/test_42sh.c
@@ -327,7 +327,15 @@ Test(shell, my_alias_print_add, .init = redirect_all_std)
     free_array(tab);
 }
 
+Test(shell, my_alias_unique_alias, .init = redirect_all_std)
+{
+    tcsh_t *term = calloc(1, sizeof(tcsh_t));
+    char **tab = my_str_to_word_array("toto tata", " \n\t");
 
+    my_alias(term, tab);
+    my_alias(term, &tab[1]);
+    free_array(tab);
+}
 
 
 Test(shell, my_alias_and_check_alias, .init = redirect_all_std)
