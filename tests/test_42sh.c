23e8406946a88a96d1fed1a81574f7589c642c73
Alessandro Paris <alessandro1.paris@epitech.eu>
2026-05-11 16:55:09 +0200

    [+] add: choose command foreach syntax path test

--- tests/test_42sh.c
+++ tests/test_42sh.c
@@ -546,4 +546,14 @@ Test(shell, choose_command_if_syntax_path, .init = redirect_all_std)
     free(term);
 }
 
+Test(shell, choose_command_foreach_syntax_path, .init = redirect_all_std)
+{
+    tcsh_t *term = calloc(1, sizeof(tcsh_t));
+    char *cmd = my_strdup("foreach x ( a b ) echo $x");
+
+    cr_assert_not_null(term);
+    cr_assert_not_null(cmd);
+    choose_command(term, cmd);
+    free(term);
+}
 
