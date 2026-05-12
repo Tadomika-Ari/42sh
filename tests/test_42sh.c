d601d89f329e3eb4c2365a9384307922815fcdd2
Alessandro Paris <alessandro1.paris@epitech.eu>
2026-05-12 22:17:02 +0200

    [+] add: parser cmd 16 test

--- tests/test_42sh.c
+++ tests/test_42sh.c
@@ -1889,3 +1889,10 @@ Test(shell, parser3000_cmd_15, .init = redirect_all_std)
     free_array(tab);
 }
 
+Test(shell, parser3000_cmd_16, .init = redirect_all_std)
+{
+    char **tab = parser3000("/bin/ls", "\n\t ");
+
+    free_array(tab);
+}
+
