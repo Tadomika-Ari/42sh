5f5128acc20f8258d6af694754ccc0612cac4371
Alessandro Paris <alessandro1.paris@epitech.eu>
2026-05-12 22:01:11 +0200

    [+] add: parser quote 3 test

--- tests/test_42sh.c
+++ tests/test_42sh.c
@@ -1756,3 +1756,10 @@ Test(shell, parser3000_quote_2, .init = redirect_all_std)
     cr_assert_null(tab);
 }
 
+Test(shell, parser3000_quote_3, .init = redirect_all_std)
+{
+    char **tab = parser3000("toto\" tata", "\n\t ");
+
+    cr_assert_null(tab);
+}
+
