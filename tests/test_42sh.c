489c905400049b022c0176574ea8a159e965b048
Alessandro Paris <alessandro1.paris@epitech.eu>
2026-05-12 22:05:34 +0200

    [+] add: parser cmd 3 test

--- tests/test_42sh.c
+++ tests/test_42sh.c
@@ -1790,3 +1790,10 @@ Test(shell, parser3000_cmd_2, .init = redirect_all_std)
 
     free_array(tab);
 }
+
+Test(shell, parser3000_cmd_3, .init = redirect_all_std)
+{
+    char **tab = parser3000("mkdir \"a b\"", "\n\t ");
+
+    free_array(tab);
+}
