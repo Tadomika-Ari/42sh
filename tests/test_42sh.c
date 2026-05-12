0fba708b62316d8480442b631fae06bed00e6fc5
Alessandro Paris <alessandro1.paris@epitech.eu>
2026-05-12 21:15:44 +0200

    [+] add: parser bracket 4 test

--- tests/test_42sh.c
+++ tests/test_42sh.c
@@ -1664,3 +1664,10 @@ Test(shell, parser3000_bracket_3, .init = redirect_all_std)
 
     cr_assert_null(tab);
 }
+
+Test(shell, parser3000_bracket_4, .init = redirect_all_std)
+{
+    char **tab = parser3000("toto (tata", "\n\t ");
+
+    cr_assert_null(tab);
+}
