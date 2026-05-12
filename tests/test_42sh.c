4389e7313fec0d9358d81ea8964c1bd2c5dbf307
Alessandro Paris <alessandro1.paris@epitech.eu>
2026-05-12 22:12:09 +0200

    [+] add: parser cmd 7 test

--- tests/test_42sh.c
+++ tests/test_42sh.c
@@ -1818,3 +1818,10 @@ Test(shell, parser3000_cmd_6, .init = redirect_all_std)
 
     free_array(tab);
 }
+
+Test(shell, parser3000_cmd_7, .init = redirect_all_std)
+{
+    char **tab = parser3000("setenv", "\n\t ");
+
+    free_array(tab);
+}
