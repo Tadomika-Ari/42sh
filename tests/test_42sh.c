62cae8d39d0cf67d2c2e033766fcf21cc1b05867
Alessandro Paris <alessandro1.paris@epitech.eu>
2026-05-12 21:58:26 +0200

    [+] add: parser single quote 4 test

--- tests/test_42sh.c
+++ tests/test_42sh.c
@@ -1728,4 +1728,11 @@ Test(shell, parser3000_single_quote_3, .init = redirect_all_std)
     cr_assert_null(tab);
 }
 
+Test(shell, parser3000_single_quote_4, .init = redirect_all_std)
+{
+    char **tab = parser3000("toto \'tata", "\n\t ");
+
+    cr_assert_null(tab);
+}
+
 
